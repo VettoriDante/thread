723785b77d7547967055e7fcfd00dc282d382e51
lai2-24 <lai2-24@itismeucci.com>
2024-09-25 12:08:06 +0200

    thread implements

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World");
+        MioThread t1 = new MioThread(10);
+        MioThread t2 = new MioThread(10);
+        new Thread(t1).start();
+        new Thread(t2).start();
+
     }
 }
